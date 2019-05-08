1986b0a35eb7c10935964e6f544796cb6a06a0f2
mavl <mvaronamac@gmail.coml>
2019-05-08 13:15:24 +0200

    Add sockect IO to the server

--- server/classes/server.ts
+++ server/classes/server.ts
@@ -1,15 +1,29 @@
 import express from 'express';
 import { SERVER_PORT } from '../globals/environment';
+import socketio from 'socket.io';
+import http from 'http';
 export default class Server {
     public app: express.Application;
-    public port: number;  
+    public port: number;
+    
+    public io: SocketIO.Server;
+    private httpServer: http.Server;
     
     constructor(){
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server(this.app);
+        this.io = socketio(this.httpServer);
+        this.listenSockets();
+    }
+
+    private listenSockets(){
+        this.io.on('connection', client => {
+            console.log('Client connected');
+        });
     }
 
     start( callback: Function){
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 }
